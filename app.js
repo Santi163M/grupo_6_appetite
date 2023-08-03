a618a6d4937516555e2c2678539ab461814b250f
GiuliBazzi <zulemanahir13@gmail.com>
2023-08-03 01:04:04 -0300

    carrito

--- app.js
+++ app.js
@@ -16,4 +16,8 @@ app.get('/productDetail', (req,res) => {
 })
 app.use(express.static('public'));
 
+app.get('/productDetail', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/carrito.html'))
+});
+
 app.listen('8000', console.log('Servidor iniciado en el puerto 8000.'))
