b857612120db454e13eac007b7a2dfc6fbbfb374
Leop1n <137943710+LeoP1n@users.noreply.github.com>
2023-08-24 21:18:52 -0300

    nuevos links

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,12 +3,12 @@ const productcontrol = require("../controll/products")
 const productrouter = express.Router()
 
 
-productrouter.get("/product",productcontrol.cart)
-productrouter.get("/productdetail",productcontrol.details)
-productrouter.get("/productdetail/:id",productcontrol.detailsn)
-productrouter.get("/editarproducto", productcontrol.editarproducto)
-productrouter.post("editarproducto", productcontrol.editarproductopost)
-productrouter.get("/crearproducto", productcontrol.crearproducto)
-productrouter.post("/crearproducto", productcontrol.crearproductopost)
 
-module.exports = productrouter 
+
+productrouter.get("/product",productcontrol.cart)
+/*
+productrouter.get("/productdetail",productcontrol.details)
+*/
+productrouter.get("/product/details/:id",productcontrol.detailsn)
+
+module.exports = productrouter
