014025938442bb1d0fdee1b2da19995d2e178602
Leop1n <137943710+LeoP1n@users.noreply.github.com>
2023-08-26 20:06:18 -0300

    rutas cambiadas

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,10 +5,13 @@ const productrouter = express.Router()
 
 
 
-productrouter.get("/product",productcontrol.cart)
-/*
-productrouter.get("/productdetail",productcontrol.details)
-*/
-productrouter.get("/product/details/:id",productcontrol.detailsn)
+productrouter.get("/products",productcontrol.cart)
+
+productrouter.get("/products/:id/details",productcontrol.detailsn)
+
+productrouter.get("/products/:id/edit",productcontrol.editarproducto)
+
+productrouter.get("/products/create",productcontrol.crearproducto)
+
 
 module.exports = productrouter
