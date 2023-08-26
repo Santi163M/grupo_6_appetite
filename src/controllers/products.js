b61479e3b0018dfb661571f747c30bbd3426a1c0
Leop1n <137943710+LeoP1n@users.noreply.github.com>
2023-08-26 20:03:38 -0300

    correcciones

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,43 +1,30 @@
-const fs = require("fs");
-const datos = fs.readFileSync("src/data/products.json","utf-8");
-const informacion = leerjson(datos);
+const fs = require("fs")
+
+//leer json
+
+const datos = fs.readFileSync("src/data/products.json","utf-8")
 
 function leerjson (data) {
-    return JSON.parse(data);
-} 
+    let convertir;
+    convertir = JSON.parse(data)
+    return convertir
+}
 
-// console.log(`Datos entrantes : `, leerjson(datos));
+//console.log(`Datos entrantes products : `, leerjson(datos))
+let informacion = leerjson(datos)
 
-const controller = {
-    cart: (req, res) => {
-        res.render("productCart", {informacion});
+const productcontrol = {
+    cart:(req,res)=>{
+        res.render("productCart",{informacion})
     },
-    
-    details: (req, res) => {
-        res.render("productDetail");
-    },
-
-    detailsn: (req ,res) => {
-        const id = req.params.id;
-        let product = informacion.find(product => product.id == id);
-        res.render("productDetail");
-    },
-
-    editarproducto:(req, res) => {
-        res.render("editarproducto");
-    },
-
-    editarproductopost: (req, res) => {
-        res.redirect("/");
-    },
-
-    crearproducto: (req, res) => {
-        res.render("crearproducto");
-    },
-
-    crearproductopost:(req, res) => {
-        res.redirect("/");
+    /* details:(req,res)=>{
+        res.render("productDetail")
+    }, */
+    detailsn: (req,res)=>{
+        const n = req.params.id
+        let productoelegido = informacion.find(p => p.id == n)
+        res.render("productDetail",{productoelegido})
     }
 }
 
-module.exports = controller;
+module.exports = productcontrol
