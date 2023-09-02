48bc3c719d619a870cb598861f5ff6250a0e89f0
Leop1n <137943710+LeoP1n@users.noreply.github.com>
2023-09-02 19:24:51 -0300

    edit y create post agregados

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,9 @@
 const express = require("express")
-const productcontrol = require("../controllers/productsControllers")
+const productcontrol = require("../controllers/products")
 const productrouter = express.Router()
 
-productrouter.get("/product",productcontrol.cart)
-productrouter.get("/productdetail",productcontrol.details)
-productrouter.get("/productdetail/:id",productcontrol.detailsn)
-productrouter.get("/editarproducto", productcontrol.editarproducto)
-productrouter.post("/editarproducto", productcontrol.editarproductopost)
-productrouter.get("/crearproducto", productcontrol.crearproducto)
-productrouter.post("/crearproducto", productcontrol.crearproductopost)
-productrouter.get("/listadeproductos", productcontrol.listadeproductos)
+
+
 
 productrouter.get("/products",productcontrol.cart)
 
@@ -17,7 +11,10 @@ productrouter.get("/products/:id/details",productcontrol.detailsn)
 
 productrouter.get("/products/:id/edit",productcontrol.editarproducto)
 
+productrouter.post("/products/:id/edit",productcontrol.editarproductopost)
+
 productrouter.get("/products/create",productcontrol.crearproducto)
 
+productrouter.post("/products/create",productcontrol.crearproductopost)
 
 module.exports = productrouter
