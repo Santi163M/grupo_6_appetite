2f06a0e982ba8e18a9061fbe1dbc022b6baa9e77
Leop1n <137943710+LeoP1n@users.noreply.github.com>
2023-11-03 20:10:19 -0300

    parte sprint 6

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require("path")
 const productosController = require('../controllers/products');
 
+
+const multer=require('multer')
+
+let storag=multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../../public/img/products'))
+    },
+    filename: (req, file, cb)=>{
+        console.log(file)
+        let newFileName='product-'+Date.now()+path.extname(file.originalname)
+        cb(null, newFileName)
+}})
+
+let upload=multer({storage:storag})
+
+
 router.get('/', productosController.productos);
-router.get('/:id', productosController.detalleProducto);
-router.get('/crear/:id', productosController.crear);
-router.post('/crear/:id', productosController.productoCreado);
+router.get('/product/:id', productosController.detalleProducto);
+router.get('/crear', productosController.crear);
+
+router.post('/crear',upload.single('foto'), productosController.productoCreado);
 router.get('/editar/:id', productosController.editar);
 router.put('/editar/:id', productosController.productoEditado);
 router.delete('/eliminar/:id', productosController.eliminarProducto);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,31 +1,37 @@
 const express = require("express")
 const usercontroller = require("../controllers/user")
 let userrouter = express.Router()
+
 const path=require('path')
+
 const multer=require('multer')
+const { check } = require("express-validator")
 
 //multer
-const storage=multer.diskStorage({
+let storage=multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, path.join(__dirname, '../../public/img/users'))
     },
     filename: (req, file, cb)=>{
         console.log(file)
-        const newFileName='user-'+Date.now()+path.extname(file.originalname)
+        let newFileName='user-'+Date.now()+path.extname(file.originalname)
         cb(null, newFileName)
 }})
 
-const upload=multer({storage:storage})
+let upload=multer({storage:storage})
 
 
 
 userrouter.get("/register",usercontroller.create)
+
 userrouter.post("/register", upload.single('foto'), usercontroller.createpost)
+
 userrouter.get("/login",usercontroller.login)
 userrouter.post("/loginpost",[
     check('email').isEmail().withMessage('Email inválido'),
     check('password').isLength({min:8}).withMessage('La contraseña debe tener al menos 8 caracteres')
 ], usercontroller.loginpost)
+
 userrouter.get("/user",usercontroller.user)
 
 module.exports = userrouter
